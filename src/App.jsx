353d1b2e4581ac854755f482475144e2ddbda25d
ds-arunkumar <arunkumar519@gmail.com>
2024-12-08 10:29:10 +0530

    React Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/header';
+import Discription from './components/discription.jsx';
+
+
+function App(){
+  return(
+    <>
+    <Header/>
+    <Discription/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/discription.jsx
@@ -0,0 +1,9 @@
+const Discription = () =>{
+  return(
+    <>
+    <p>I am saying Hi to the World!</p>
+    </>
+  )
+}
+
+export default Discription;
